b8652b07f07846064684a9c7c64aed8cd08ffa50
Sangho Kim <marchbreeze03@gmail.com>
2024-02-04 23:44:39 +0900

    [FEAT] 페어링 기기 확인

--- app/src/main/java/march/breeze/locationbluetooth/connect/ConnectActivity.kt
+++ app/src/main/java/march/breeze/locationbluetooth/connect/ConnectActivity.kt
@@ -1,11 +1,17 @@
 package march.breeze.locationbluetooth.connect
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.ParcelUuid
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import march.breeze.locationbluetooth.R
 import march.breeze.locationbluetooth.databinding.ActivityConnectBinding
 import march.breeze.locationbluetooth.util.base.BaseActivity
@@ -19,14 +25,22 @@ class ConnectActivity() : BaseActivity<ActivityConnectBinding>(R.layout.activity
 
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
+    private var pairedDeviceName = ""
+    private var pairedDeviceMACAddress = ""
+    private lateinit var pairedDeviceUUID: ParcelUuid
+
+    private var isPermitted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        initBluetoothStartBtnListener()
+        initStartBtnListener()
         initBluetoothActivateCallback()
+        initPairedDeviceBtnListener()
+        checkBluetoothPermission()
     }
 
-    private fun initBluetoothStartBtnListener() {
+    private fun initStartBtnListener() {
         binding.btnStartBluetooth.setOnSingleClickListener {
             initBluetoothAdapter()
         }
@@ -73,8 +87,62 @@ class ConnectActivity() : BaseActivity<ActivityConnectBinding>(R.layout.activity
         }
     }
 
+    private fun initPairedDeviceBtnListener() {
+        binding.btnShowPairedDevice.setOnSingleClickListener {
+            getPairedDevices()
+        }
+    }
+
+    private fun getPairedDevices() {
+        checkBluetoothPermission()
+        bluetoothAdapter?.let {
+            if (it.isEnabled) {
+                val pairedDevices: Set<BluetoothDevice> = it.bondedDevices
+                if (pairedDevices.isNotEmpty()) {
+                    pairedDevices.forEach { device ->
+                        pairedDeviceName = device.name
+                        pairedDeviceMACAddress = device.address
+                        pairedDeviceUUID = device.uuids[0]
+                    }
+                } else {
+                    toast("페어링된 기기가 없습니다")
+                }
+            } else {
+                toast("블루투스가 비활성화되어 있습니다.")
+            }
+        }
+    }
+
+    private fun checkBluetoothPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.BLUETOOTH_CONNECT
+            ) != PackageManager.PERMISSION_GRANTED && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), PERMIT_REQUEST
+            )
+        } else {
+            isPermitted = true
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMIT_REQUEST) {
+            isPermitted =
+                grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         bluetoothAdapter = null
+    }
+
+    companion object {
+        private const val PERMIT_REQUEST = 0
     }
 }
