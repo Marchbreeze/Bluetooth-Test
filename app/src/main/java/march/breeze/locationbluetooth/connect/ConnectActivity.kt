4e0a1491af0aca0842bfabb8d761f77c442333f8
Sangho Kim <marchbreeze03@gmail.com>
2024-02-04 21:38:52 +0900

    [ADD] 블루투스 권한 설정

--- app/src/main/java/march/breeze/locationbluetooth/connect/ConnectActivity.kt
+++ app/src/main/java/march/breeze/locationbluetooth/connect/ConnectActivity.kt
@@ -1,12 +1,14 @@
 package march.breeze.locationbluetooth.connect
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import march.breeze.locationbluetooth.R
+import march.breeze.locationbluetooth.databinding.ActivityConnectBinding
+import march.breeze.locationbluetooth.util.base.BaseActivity
 
-class ConnectActivity : AppCompatActivity() {
+class ConnectActivity() : BaseActivity<ActivityConnectBinding>(R.layout.activity_connect) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_connect)
     }
+
 }
