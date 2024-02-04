aa5e9998b65f508783e2f1d93efb5ede0a066f9f
Sangho Kim <marchbreeze03@gmail.com>
2024-02-04 20:25:45 +0900

    [MOVE] 패키지 구조 수정

---
+++ app/src/main/java/march/breeze/locationbluetooth/connect/ConnectActivity.kt
@@ -0,0 +1,12 @@
+package march.breeze.locationbluetooth.connect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import march.breeze.locationbluetooth.R
+
+class ConnectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connect)
+    }
+}
